d2a16ba8bb1de19b906781dbaca42deec837031b
Ken Wu <kenwu1009us@gmail.com>
2023-03-07 00:14:38 -0800

    set the frame

---
+++ src/com/snake/Main.java
@@ -0,0 +1,19 @@
+package com.snake;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setBounds(10, 10,900, 720);
+        //windows size cannot be changed
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+        frame.add(new GamePanel());
+
+        frame.setVisible(true);
+    }
+}
